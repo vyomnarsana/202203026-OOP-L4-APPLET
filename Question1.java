0963770d3999b4098a2939281ab0235e02d50d3b
vyomnarsana <122280072+vyomnarsana@users.noreply.github.com>
2023-04-22 13:33:14 +0530

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -11,13 +11,8 @@ public class Question1 extends Applet {
     public void init() {
 
         t1 = new TextField(10);
-//        System.out.println("Enter your name: ");
-
           add(t1);
-        //str1 = t1.getText();
-//        String str1;
-//        str1 = t1.getText();
-//        greeting = "Hello "+str1;
+
     }
 
     public void paint(Graphics g) {
@@ -26,8 +21,6 @@ public class Question1 extends Applet {
           String str1;
           str1 = t1.getText();
           String greeting = "Hello "+str1;
-          //System.out.println("Enter your name: ");
-
 
         Dimension dimension = getSize();
 
